3b111baa6f23c0fb83245e967d9aa3f3161c377a
baoluan123 <huynhphanbaoluan456@gmail.com>
2026-04-18 11:38:09 +0700

    Initial commit: Bat dau xay lai do an

---
+++ agriTech/src/main/java/com/example/agriTech/AgriTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.agriTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriTechApplication.class, args);
+	}
+
+}
---
+++ agriTech/src/test/java/com/example/agriTech/AgriTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agriTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
