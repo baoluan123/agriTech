8d69ca093a5f7341a8f9c2223028c4f36c6801cd
baoluan123 <huynhphanbaoluan456@gmail.com>
2026-04-22 23:42:43 +0700

    AGRI-37: OLED thanh cong+Post du lieu ve sever

--- AgriTech_ESP32_Core/src/main.cpp
+++ AgriTech_ESP32_Core/src/main.cpp
@@ -1,27 +1,71 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include  <ArduinoJson.h>
+const char* ssid = "P325-2";
+const char* pass = "@123456789";
+
+const char* ip = "http://192.168.1.8:8080/api/sensor/moisture";
+
+const int pinAnalog = 34; //A0
+const int pinDigital = 25;//D0
+
+
 
 void setup() {
   Serial.begin(115200);
-  delay(2000); // Đợi 2 giây cho ổn định
-  Serial.println(">>> BUOC 1: Kiem tra Serial OK");
-
-  Wire.begin(); 
-  Serial.println(">>> BUOC 2: Khoi tao I2C OK");
-
-  // Quét thử xem có thiết bị nào đang cắm vào I2C không
-  byte error, address;
-  Serial.println(">>> BUOC 3: Dang quet thiet bi I2C...");
+  WiFi.begin(ssid,pass);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  pinMode(pinDigital,INPUT); // D0 là chân nhập tín hiệu số
+  Serial.println("\nWifi đã kết nối!");
   
-  address = 0x3C; // Địa chỉ OLED
-  Wire.beginTransmission(address);
-  error = Wire.endTransmission();
-
-  if (error == 0) Serial.println(">>> KET QUA: Tim thay OLED tai 0x3C!");
-  else Serial.println(">>> KET QUA: KHONG tim thay OLED!");
 }
 
 void loop() {
-  Serial.print(".");
-  delay(1000);
+  if(WiFi.status() == WL_CONNECTED) {
+    HTTPClient Client; // Nen dung chu thuong cho ten doi tuong
+    Client.begin(ip);
+    Client.addHeader("Content-Type", "application/json");
+    // Doc gia tri cam bien
+    // doc gia tri A0
+  int giaTriAlog = analogRead(pinAnalog);
+  // doc trang thai  0 / 1
+  int giaDiaLog = digitalRead(pinDigital);
+// Dong goi JSON (Dung JsonDocument cho phien ban moi) hoac  StaticJsonDocument
+  JsonDocument doc;
+  doc["sensorId"] = "ESP32_GARDEN_01";
+  doc["value"] = giaTriAlog;
+  doc["status"] = (giaDiaLog == HIGH) ? "KHO" : "UOT"; // Gui luon trang thai qua cho ngau
+  String requestBody;
+  serializeJson(doc,requestBody);
+
+  // Gui du lieu
+  int HttpResponse = Client.POST(requestBody);
+  if (HttpResponse > 0) {
+      Serial.printf("Gửi thành công, Code: %d\n", HttpResponse,giaTriAlog);
+    } else {
+      Serial.printf("Lỗi gửi dữ liệu: %s\n",Client.errorToString(HttpResponse).c_str());
+    }
+    Client.end();
+
+  
+  } else {
+    Serial.println("Mat ket noi Wifi, dang thu lai...");
+    WiFi.begin(ssid, pass);
+  }
+  delay(10000);
 }
+
+ // in ra so sanh du lieu
+  // Serial.print("Dữ liệu (A0): ");
+  // Serial.print(giaTriAlog);
+  // Serial.print(" | Báo động (D0): ");
+  // if(giaDiaLog == HIGH) {
+  //   Serial.println("-> [!] DAT DANG KHO");
+  // } else {
+  //   Serial.println("-> [!] DAT DANG uot");
+  // }
+  // }
